8d5172b491a05e16f99e23af01a173fe70acbaa7
Michael <michael.sim132@gmail.com>
2020-09-05 21:29:02 -0400

    fixed navbar on homepage

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -32,7 +32,7 @@ class Navbar extends Component {
 
     render() {
         return(
-            <nav className={!this.state.scrolledPast && window.location.pathname === process.env.PUBLIC_URL+'/#/' ? 'NavbarItems' : 'NavbarItemsColored'}>
+            <nav className={!this.state.scrolledPast && this.props.transparent ? 'NavbarItems' : 'NavbarItemsColored'}>
                 <div className="menu-icon" onClick={this.handleClick}>
                     <i className={this.state.clicked ? 'fas fa-times': 'fas fa-bars'}></i>
                 </div>
@@ -50,6 +50,10 @@ class Navbar extends Component {
             </nav>
         )
     }
+}
+
+Navbar.defaultProps = {
+    transparent: false
 }
 
 export default Navbar;
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -10,7 +10,7 @@ const MainPage = () => {
         return(
             <div>
                 <FadeIn>
-                <Navbar />
+                <Navbar transparent={true} />
                 </FadeIn>
                 <div className='page-container'>
                 <div className='content-wrap-image'>
